79850c8d42d459682aed2658ab5ab19e7e558026
Dylan Wood <dwood@mrn.org>
2015-11-11 13:28:23 +1100

    use postgres user credentials

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 'use strict';
 var path = require('path');
+
+/*script currently uses connects as the postgres user from localhost, so no
+    credentials are necessary
 var getConnInfo = require(path.join(__dirname, 'lib/get-connection-info.js'));
 var connectionInfo = getConnInfo();
 connectionInfo.database = connectionInfo.db;
 connectionInfo.user = connectionInfo.username;
+*/
+var connectionInfo = {
+    host: 'localhost',
+    user: 'postgres'
+};
 var knex = require('knex')({
     client: 'pg',
     connection: connectionInfo
