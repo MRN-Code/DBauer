6da1027bab9b01216796792aa1d366b09a05196f
Dylan <dylancwood@gmail.com>
2015-11-26 17:49:50 -0700

    unstaged changes from yester-year

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 'use strict';
-var config = require('config');
 var path = require('path');
-var tempDbName = config.get('tempDbName');
 var dbUtils = require(path.join(__dirname, 'lib/db-utils.js'));
 
 var getConnInfo = require(path.join(__dirname, 'lib/get-connection-info.js'));
 var connectionInfo = getConnInfo();
-
+    
 dbUtils.init(connectionInfo);
-//dbUtils.copySchema(tempDbName);
+dbUtils.copySchema();
 var copyData = require(path.join(__dirname, 'lib/copy-seed-data.js'));
 copyData(connectionInfo)
     .then(function() {
