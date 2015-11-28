e3db2eacf64974138995e348a078f207566e5253
Dylan Wood <dwood@mrn.org>
2015-11-27 19:46:27 -0800

    doc blocks and better promise flow

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ var s3Utils = require(path.join(__dirname, 'lib/s3-utils.js'));
 
 var getConnInfo = require(path.join(__dirname, 'lib/get-connection-info.js'));
 var connectionInfo = getConnInfo();
+var copyData = require(path.join(__dirname, 'lib/copy-seed-data.js'));
+
+function closeShop() {
+    process.exit(0);
+}
 
 dbUtils.init(connectionInfo);
 dbUtils.copySchema();
-var copyData = require(path.join(__dirname, 'lib/copy-seed-data.js'));
 copyData(connectionInfo)
-    .then(function() {
-        dbUtils.dumpTempDb();
-        return s3Utils.upload()
-            .then(function() {
-                dbUtils.dropTempDb();
-                process.exit(0);
-            })
-            .catch(function(err) {
-                console.log('Error during upload or dropTempDb');
-                console.dir(err);
-            });
+    .then(dbUtils.dumpTempDb)
+    .then(s3Utils.upload)
+    .then(dbUtils.dropTempDb)
+    .then(closeShop)
+    .catch(function(err) {
+        console.log('Error during upload or dropTempDb');
+        console.dir(err);
     });
